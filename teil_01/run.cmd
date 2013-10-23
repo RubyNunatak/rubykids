4b43011f060434e8dc50dfde6a14c2f9cd2bd2c3
Frithjof <eckhardt.f@googlemail.com>
2013-10-23 10:12:06 +0200

    Skripte fuer die Kommandozeile, damit man beim Testen der Lektionen nicht so viel tippen muss

--- teil_01/run.cmd
+++ teil_01/run.cmd
@@ -1,2 +1,54 @@
 @echo off
-ruby lektion_%1.rb
+rem Copyright (C) 2007-2013 www.rubykids.de Frithjof Eckhardt
+rem Alle Rechte vorbehalten.
+
+if "%1"=="" goto usage
+SET LEKTION_PREFIX=lektion_
+SET LEKTION_NR=%1
+
+rem UTF-8 Codepage
+set UTF8CP=65001
+
+setlocal EnableExtensions
+setlocal EnableDelayedExpansion
+
+rem Alte Codepage merken
+set CP_OLD=
+for /f "tokens=1 delims==" %%i in ('chcp') do (
+  set CP_OLD=%%i
+  set CP_OLD=!CP_OLD:Aktive Codepage:=!
+  set CP_OLD=!CP_OLD:~1,-1!
+)
+
+rem UTF-8 Codepage für Ausgabe setzen
+chcp %UTF8CP% > nul
+
+rem Gewünschte Lektion mit Ruby-Interpreter aufrufen
+ruby %LEKTION_PREFIX%%LEKTION_NR%.rb
+
+chcp %CP_OLD% > nul
+rem ... und wieder auf vorherige Codepage einstellen
+
+endlocal
+goto end
+
+:usage
+echo.Ruft den Rubyinterpreter fuer eine Lektion auf (www.rubykids.de).
+echo.
+echo run nn
+echo  nn - die Nummer einer Lektion
+echo.
+echo Beispiele: 
+set example1=01
+set example2=07
+echo.
+echo   run %example1%
+echo   run %example2%
+echo.
+echo Ruft die Lektionen %example1%, oder %example2% auf und entspricht den Befehlen: 
+echo.
+echo   ruby %LEKTION_PREFIX%%example1%.rb
+echo   ruby %LEKTION_PREFIX%%example2%.rb
+echo.
+
+:end
--- teil_02/run.cmd
+++ teil_02/run.cmd
@@ -1,2 +1,57 @@
 @echo off
-ruby theorie_%1.rb
+rem Copyright (C) 2007-2013 www.rubykids.de Frithjof Eckhardt
+rem Alle Rechte vorbehalten.
+
+if "%1"=="" goto usage
+SET LEKTION_PREFIX=theorie_
+SET LEKTION_NR=%1
+
+rem UTF-8 Codepage
+set UTF8CP=65001
+
+setlocal EnableExtensions
+setlocal EnableDelayedExpansion
+
+rem Alte Codepage merken
+set CP_OLD=
+for /f "tokens=1 delims==" %%i in ('chcp') do (
+  set CP_OLD=%%i
+  set CP_OLD=!CP_OLD:Aktive Codepage:=!
+  set CP_OLD=!CP_OLD:~1,-1!
+)
+
+rem UTF-8 Codepage für Ausgabe setzen
+chcp %UTF8CP% > nul
+
+rem Gewünschte Lektion mit Ruby-Interpreter aufrufen
+ruby %LEKTION_PREFIX%%LEKTION_NR%.rb
+
+chcp %CP_OLD% > nul
+rem ... und wieder auf vorherige Codepage einstellen
+
+endlocal
+goto end
+
+:usage
+echo.Ruft den Rubyinterpreter fuer eine Lektion auf (www.rubykids.de).
+echo.
+echo run nn
+echo  nn - die Nummer einer Theorielektion
+echo.
+echo Beispiele: 
+set example1=05
+set example2=05_aufgaben
+set example3=06_a
+echo.
+echo   run %example1%
+echo   run %example2%
+echo   run %example3%
+echo.
+echo Ruft die Theorielektionen %example1%, %example2%, oder %example3% auf und entspricht den Befehlen: 
+echo.
+echo   ruby %LEKTION_PREFIX%%example1%.rb
+echo   ruby %LEKTION_PREFIX%%example2%.rb
+echo   ruby %LEKTION_PREFIX%%example3%.rb
+echo.
+
+:end
